b0910e42adbb410a25670d012f22ee416f57d7f4
HippoMaru <hippomaru@ya.ru>
2024-01-26 20:57:55 +0300

    I'm tired but that's okay... actually not, I'm emotionally exhausted with this editor. I could do so much, but I have to refactor this code again just because of... never mind, I will cope with it

--- src/main/java/com/hex/HEXEditor.java
+++ src/main/java/com/hex/HEXEditor.java
@@ -6,9 +6,14 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HEXEditor {
     private JFrame mainJFrame;
+    private int nColumns = 10;
+
+    private MainTable mainTable;
 
     private RandomAccessFile file;
 
@@ -29,7 +34,46 @@ public class HEXEditor {
                 updateFile();
             }
         });
+
+        JTextField searchTF = new JTextField(30);
+        JButton searchButton = createSearchButton(searchTF);
         return jFrame;
+    }
+
+    private JButton createSearchButton(JTextField searchTF){
+        JButton searchButton = new JButton("Search");
+        searchButton.addActionListener(e  -> {
+            String searchInput = searchTF.getText();
+            Pattern pattern = Pattern.compile(searchInput);
+            Matcher matcher;
+            StringBuilder dataLine;
+            int startIndex;
+            int endIndex;
+            try {
+                for (long i = 0; i < Math.ceil((double) file.length() / nColumns); i++) {
+                    dataLine = new StringBuilder();
+                    file.seek(nColumns * i);
+                    byte[] line = new byte[nColumns];
+                    file.read(line);
+                    for (byte b : line) {
+                        dataLine.append(String.format("%02X", b));
+                    }
+                    matcher = pattern.matcher(dataLine);
+                    if (matcher.find()) {
+                        startIndex = matcher.start();
+                        endIndex = matcher.end();
+                        JTable table = mainTable.getTable();
+                        table.setRowSelectionInterval((int) i, (int) i);
+                        table.setColumnSelectionInterval(startIndex / 2, (endIndex - 1) / 2);
+                        table.scrollRectToVisible(table.getCellRect((int) i, startIndex / 2, true));
+                        mainTable.setTable(table);
+                        break;
+                    }
+                }
+            }
+            catch (IOException ignored){}
+        });
+        return searchButton;
     }
 
     private void updateFile() {
