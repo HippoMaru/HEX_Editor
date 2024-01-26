bd3fdea4c4b863d89fa411b1ecea6cd2d5a0d0fb
HippoMaru <hippomaru@ya.ru>
2024-01-26 19:37:09 +0300

    HEXEditor class scratched

---
+++ src/main/java/com/hex/HEXEditor.java
@@ -0,0 +1,61 @@
+package com.hex;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.util.*;
+
+public class HEXEditor {
+    private JFrame mainJFrame;
+
+    private RandomAccessFile file;
+
+    public HEXEditor() {
+        this.mainJFrame = createMainJFrame();
+    }
+
+
+
+
+    public JFrame createMainJFrame(){
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds((dimension.width - 1000)/2, (dimension.height - 300)/2, 1000, 300);
+        jFrame.setTitle("HEX Editor 2.0 by HippoMaru");
+        jFrame.addWindowListener(new WindowAdapter(){
+
+            public void windowClosing(WindowEvent e){
+                try {
+                    updateFile();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+        return jFrame;
+    }
+
+    private void updateFile(){};
+
+    public void run(){}
+
+    public JFrame getMainJFrame() {
+        return mainJFrame;
+    }
+
+    public void setMainJFrame(JFrame mainJFrame) {
+        this.mainJFrame = mainJFrame;
+    }
+
+    public RandomAccessFile getFile() {
+        return file;
+    }
+
+    public void setFile(RandomAccessFile file) {
+        this.file = file;
+    }
+}
